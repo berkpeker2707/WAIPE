47f5c58f0d24b6d66c42a684759ae6b193933895
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2022-05-04 18:02:08 +0300

    Add routes

--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
 
+const {
+  getProfile,
+  getPetProfile,
+  postPetProfile,
+  postComment,
+} = require("../controllers/userControllers");
+
 const userRoutes = express.Router();
 
-const { getExampleController } = require("../controllers/userControllers");
-
-userRoutes.get("/get/example", getExampleController);
-
 module.exports = userRoutes;
+
+userRoutes.get("/profile", getProfile);
+userRoutes.get("/pet/:id", getPetProfile);
+userRoutes.post("/new/pet", postPetProfile);
+userRoutes.post("/new/comment", postComment);
