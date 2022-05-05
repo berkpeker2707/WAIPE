f3dcb1a32efa777fa4b00684dbefced47dd7eb7e
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2022-05-05 11:11:47 +0300

    Add past and sender

--- api/models/comment.js
+++ api/models/comment.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose");
 const CommentSchema = new mongoose.Schema({
   commentTex: { type: String, required: true },
   like: { type: Array },
+  post: { type: mongoose.Schema.Types.ObjectId, ref: "post" },
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
 });
 
 module.exports = Comment = mongoose.model("Comment", CommentSchema);
