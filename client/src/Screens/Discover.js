595f8ea8d27b5a2fda9749076fdccd3f50a860a1
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2022-07-27 15:52:36 +0300

    Add button and navigation function

--- client/src/Screens/Discover.js
+++ client/src/Screens/Discover.js
@@ -1,11 +1,14 @@
 import React from "react";
-import { NativeBaseProvider, Center, Text } from "native-base";
+import { NativeBaseProvider, Center, Text, Button } from "native-base";
 
-const DiscoverScreen = () => {
+const DiscoverScreen = ({ navigation }) => {
   return (
     <NativeBaseProvider>
       <Center flex={1} px="3">
         <Text>Discover Page</Text>
+        <Button onPress={() => navigation.navigate("MainProfile")}>
+          Main Profile
+        </Button>
       </Center>
     </NativeBaseProvider>
   );
