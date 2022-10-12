ceee6217b17fbc79663d1f1e3e27b6f4c0ce6bfc
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2022-10-12 15:32:10 +0300

    Add pet list

--- client/src/Screens/MainProfile.js
+++ client/src/Screens/MainProfile.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { View } from "react-native";
 import { SimpleLineIcons } from "@expo/vector-icons";
 import {
@@ -14,6 +14,9 @@ import {
   Icon,
   ZStack,
   ScrollView,
+  HStack,
+  VStack,
+  Pressable,
 } from "native-base";
 import {
   getUser,
@@ -25,14 +28,40 @@ import { useSelector, useDispatch } from "react-redux";
 const MainProfileScreen = ({ token }) => {
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getUser(token));
-  }, [dispatch]);
-
   const currentUser = useSelector(selectCurrentUser);
   const userLoading = useSelector(selectUserLoading);
+  const [pets, setPets] = useState();
 
-  console.log(currentUser.pets);
+  const petsMatrix = () => {
+    const petsOfUser = currentUser.pets;
+    let rows = [];
+    let col = [];
+
+    petsOfUser.forEach((pet, index) => {
+      col.push(pet);
+      if ((index + 1) % 3 === 0) {
+        rows.push(col);
+        col = [];
+      }
+    });
+
+    if (col.length) {
+      console.log(col);
+      if (col.length % 3 !== 0) {
+        col.push("end");
+      }
+      rows.push(col);
+    } else {
+      rows.push(["end"]);
+    }
+
+    return rows;
+  };
+
+  useEffect(() => {
+    dispatch(getUser(token));
+    setPets(petsMatrix());
+  }, [dispatch]);
 
   return (
     <View style={style.container}>
@@ -42,7 +71,7 @@ const MainProfileScreen = ({ token }) => {
             <Spinner color={"mustard.400"} size="lg" />
           ) : (
             <>
-              <Box w="155%" h="5%" mt={10}>
+              <Box w={360} h="5%" mt={10}>
                 <Button
                   borderRadius="25"
                   alignSelf="flex-end"
@@ -108,17 +137,91 @@ const MainProfileScreen = ({ token }) => {
                 bg="trueGray.50"
                 rounded="xl"
                 height="10%"
-                width="120%"
+                w={330}
                 padding="3"
                 shadow={1}
-                mb={12}
+                mb={5}
               >
                 <Text>
                   City, Country{"\n"}
                   {currentUser.biography}
                 </Text>
               </Box>
-              <Box w="155%" h="35%" bg="trueGray.50"></Box>
+              <Box w={330} h="40%">
+                <ScrollView w={400} h="80">
+                  {pets?.map((petRow, index) => {
+                    return (
+                      <HStack
+                        space={3}
+                        flex="1"
+                        key={index}
+                        justifyContent="flex-start"
+                      >
+                        {petRow?.map((pet, petIndex) => {
+                          return (
+                            <VStack space={2} key={petIndex}>
+                              <Box w={112}>
+                                {pet !== "end" ? (
+                                  <Pressable
+                                    onPress={() => console.log("I'm Pressed")}
+                                  >
+                                    {({ isPressed }) => {
+                                      return (
+                                        <View>
+                                          <Avatar
+                                            bg="purple.600"
+                                            alignSelf="center"
+                                            width={isPressed ? 105 : 110}
+                                            height={isPressed ? 105 : 110}
+                                            shadow={1}
+                                            source={{
+                                              uri: "https://images.unsplash.com/photo-1510771463146-e89e6e86560e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80",
+                                            }}
+                                          >
+                                            {pet.name[0]}
+                                          </Avatar>
+                                          <Text
+                                            isTruncated
+                                            style={{
+                                              textAlign: "center",
+                                            }}
+                                            w={79}
+                                            alignSelf="center"
+                                            fontSize="xs"
+                                          >
+                                            {pet.name}
+                                          </Text>
+                                        </View>
+                                      );
+                                    }}
+                                  </Pressable>
+                                ) : (
+                                  <Button
+                                    borderRadius="70"
+                                    variant="ghost"
+                                    colorScheme="warning"
+                                    alignSelf="center"
+                                    width={112}
+                                    height={112}
+                                    leftIcon={
+                                      <Icon
+                                        as={SimpleLineIcons}
+                                        name="plus"
+                                        size="5xl"
+                                        color="trueGray.50"
+                                      />
+                                    }
+                                  />
+                                )}
+                              </Box>
+                            </VStack>
+                          );
+                        })}
+                      </HStack>
+                    );
+                  })}
+                </ScrollView>
+              </Box>
             </>
           )}
         </Center>
