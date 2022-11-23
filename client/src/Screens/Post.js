81b6d0081c9e31f68cd7c9ce4fbe1411a4120fc7
Berk <berkpeker94@gmail.com>
2022-11-23 09:47:55 +0300

    fixed styling of like icon and post screen more

--- client/src/Screens/Post.js
+++ client/src/Screens/Post.js
@@ -37,19 +37,8 @@ const PostScreen = ({ navigation, route }) => {
           maxW="80"
           rounded="lg"
           overflow="hidden"
-          borderColor="coolGray.200"
+          borderColor="#3a6b35"
           borderWidth="1"
-          _dark={{
-            borderColor: "coolGray.600",
-            backgroundColor: "gray.700",
-          }}
-          _web={{
-            shadow: 2,
-            borderWidth: 0,
-          }}
-          _light={{
-            backgroundColor: "gray.50",
-          }}
         >
           <Box>
             <AspectRatio w="100%" ratio={1 / 1}>
@@ -63,169 +52,133 @@ const PostScreen = ({ navigation, route }) => {
             </AspectRatio>
           </Box>
         </Box>
-        <Stack p="4" space={3}>
-          <Text fontWeight="400">{postState.postDescription ?? ""}</Text>
-          <HStack alignItems="center" space={4} justifyContent="space-between">
-            <HStack alignItems="center">
-              <Circle size="40px" bg="secondary.400">
-                <Icon
-                  as={<LikeHeartIcon name="add" size={26} />}
-                  color="white"
-                  size={5}
-                />
-              </Circle>
-              <Circle size="40px" bg="secondary.400">
-                <Icon
-                  as={<LikeHeartIcon name="add" size={26} />}
-                  color="white"
-                  size={5}
-                />
-              </Circle>
-              <Circle size="40px" bg="secondary.400">
-                <Icon
-                  as={<LikeHeartIcon name="add" size={26} />}
-                  color="white"
-                  size={5}
-                />
-              </Circle>
-              <Circle size="40px" bg="secondary.400">
-                <Icon
-                  as={<LikeHeartIcon name="add" size={26} />}
-                  color="white"
-                  size={5}
-                />
-              </Circle>
-              <Circle size="40px" bg="secondary.400">
-                <Icon
-                  as={<LikeHeartIcon name="add" size={26} />}
-                  color="white"
-                  size={5}
-                />
-              </Circle>
-            </HStack>
-            <HStack alignItems="center">
-              <Center
-                bg="primary.400"
-                _text={{
-                  color: "white",
-                  fontWeight: "bold",
-                }}
-              >
-                {postState.postDescription ?? ""}
-              </Center>
-              <Circle size="40px" bg="secondary.400">
-                <Icon
-                  as={<ProfileIcon name="Profile" size={26} />}
-                  color="white"
-                  size={5}
-                />
-              </Circle>
-            </HStack>
-          </HStack>
-          <HStack>
-            <HStack alignItems="center">
-              <Center
-                bg="primary.400"
-                _text={{
-                  color: "white",
-                  fontWeight: "bold",
-                }}
-              >
-                {postState.postDescription ?? ""}
-              </Center>
-            </HStack>
-          </HStack>
-          <Box>
-            <Divider my={1} />
-          </Box>
-          <HStack>
-            <HStack style={{ flex: 1, flexWrap: "wrap" }} space={2}>
-              <HStack>
-                <Circle size="40px" bg="secondary.400">
-                  <Icon
-                    as={<ProfileIcon name="Profile" size={26} />}
-                    color="white"
-                    size={5}
-                  />
+        <Box>
+          <Stack p="3" space={5}>
+            <HStack
+              alignItems="center"
+              space={4}
+              justifyContent="space-between"
+            >
+              <HStack alignItems="center">
+                <Circle size="30px" bg="#3a6b35">
+                  <Icon as={<LikeHeartIcon />} color="black" />
                 </Circle>
+                <Circle size="30px" bg="#3a6b35">
+                  <Icon as={<LikeHeartIcon />} color="white" />
+                </Circle>
+                <Circle size="30px" bg="#3a6b35">
+                  <Icon as={<LikeHeartIcon />} color="white" />
+                </Circle>
+                <Circle size="30px" bg="#3a6b35">
+                  <Icon as={<LikeHeartIcon />} color="white" />
+                </Circle>
+                <Circle size="30px" bg="#3a6b35">
+                  <Icon as={<LikeHeartIcon />} color="white" />
+                </Circle>
+              </HStack>
+              <HStack alignItems="center">
                 <Center
-                  bg="primary.400"
                   _text={{
-                    color: "white",
+                    color: "black",
+                    fontWeight: "bold",
+                  }}
+                >
+                  {"Lulu "}
+                </Center>
+                <Circle size="30px" bg="#3a6b35">
+                  <Icon as={<ProfileIcon name="Profile" />} />
+                </Circle>
+              </HStack>
+            </HStack>
+            <HStack>
+              <HStack alignItems="center">
+                <Center
+                  _text={{
+                    color: "black",
                     fontWeight: "bold",
                   }}
                 >
                   {postState.postDescription ?? ""}
                 </Center>
               </HStack>
-              <HStack>
-                <Text bg="primary.400">
-                  Aenean quis efficitur orci. Nullam commodo sodales massa ac
-                  tincidunt. Sed sagittis ac lacus et posuere. Aenean quis
-                  efficitur orci.
-                </Text>
+            </HStack>
+            <Box>
+              <Divider my={1} />
+            </Box>
+            <HStack>
+              <HStack style={{ flex: 1, flexWrap: "wrap" }} space={2}>
+                <HStack>
+                  <Circle size="30px" bg="#3a6b35">
+                    <Icon as={<ProfileIcon name="Profile" />} />
+                  </Circle>
+                  <Center
+                    _text={{
+                      color: "black",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {postState.postDescription ?? ""}
+                  </Center>
+                </HStack>
+                <HStack>
+                  <Text bg="#fff9">
+                    Aenean quis efficitur orci. Nullam commodo sodales massa ac
+                    tincidunt. Sed sagittis ac lacus et posuere. Aenean quis
+                    efficitur orci.
+                  </Text>
+                </HStack>
               </HStack>
             </HStack>
-          </HStack>
-          <HStack>
-            <HStack style={{ flex: 1, flexWrap: "wrap" }} space={2}>
-              <HStack>
-                <Circle size="40px" bg="secondary.400">
-                  <Icon
-                    as={<ProfileIcon name="Profile" size={26} />}
-                    color="white"
-                    size={5}
-                  />
-                </Circle>
-                <Center
-                  bg="primary.400"
-                  _text={{
-                    color: "white",
-                    fontWeight: "bold",
-                  }}
-                >
-                  {postState.postDescription ?? ""}
-                </Center>
-              </HStack>
-              <HStack>
-                <Text bg="primary.400">
-                  Aenean quis efficitur orci. Nullam commodo sodales massa ac
-                  tincidunt. Sed sagittis ac lacus et posuere. Aenean quis
-                  efficitur orci.
-                </Text>
+            <HStack>
+              <HStack style={{ flex: 1, flexWrap: "wrap" }} space={2}>
+                <HStack>
+                  <Circle size="30px" bg="#3a6b35">
+                    <Icon as={<ProfileIcon name="Profile" />} />
+                  </Circle>
+                  <Center
+                    _text={{
+                      color: "black",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {postState.postDescription ?? ""}
+                  </Center>
+                </HStack>
+                <HStack>
+                  <Text bg="#fff9">
+                    Aenean quis efficitur orci. Nullam commodo sodales massa ac
+                    tincidunt. Sed sagittis ac lacus et posuere. Aenean quis
+                    efficitur orci.
+                  </Text>
+                </HStack>
               </HStack>
             </HStack>
-          </HStack>
-          <HStack>
-            <HStack style={{ flex: 1, flexWrap: "wrap" }} space={2}>
-              <HStack>
-                <Circle size="40px" bg="secondary.400">
-                  <Icon
-                    as={<ProfileIcon name="Profile" size={26} />}
-                    color="white"
-                    size={5}
-                  />
-                </Circle>
-                <Center
-                  bg="primary.400"
-                  _text={{
-                    color: "white",
-                    fontWeight: "bold",
-                  }}
-                >
-                  {postState.postDescription ?? ""}
-                </Center>
-              </HStack>
-              <HStack>
-                <Text bg="primary.400">
-                  Aenean quis efficitur orci. Nullam commodo sodales massa ac
-                  tincidunt. Sed sagittis ac lacus et posuere. Aenean quis
-                  efficitur orci.
-                </Text>
+            <HStack>
+              <HStack style={{ flex: 1, flexWrap: "wrap" }} space={2}>
+                <HStack>
+                  <Circle size="30px" bg="#3a6b35">
+                    <Icon as={<ProfileIcon name="Profile" />} />
+                  </Circle>
+                  <Center
+                    _text={{
+                      color: "black",
+                      fontWeight: "bold",
+                    }}
+                  >
+                    {postState.postDescription ?? ""}
+                  </Center>
+                </HStack>
+                <HStack>
+                  <Text bg="#fff9">
+                    Aenean quis efficitur orci. Nullam commodo sodales massa ac
+                    tincidunt. Sed sagittis ac lacus et posuere. Aenean quis
+                    efficitur orci.
+                  </Text>
+                </HStack>
               </HStack>
             </HStack>
-          </HStack>
-        </Stack>
+          </Stack>
+        </Box>
       </Box>
     </ScrollView>
   ) : (
