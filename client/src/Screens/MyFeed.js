86cc134a5137330b95ddc283822316cb6a5e2510
Berk <berkpeker94@gmail.com>
2022-11-28 11:02:36 +0300

    added myfeed screen

---
+++ client/src/Screens/MyFeed.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useMemo, useState } from "react";
+import { View, StyleSheet } from "react-native";
+import {
+  ScrollView,
+  Center,
+  Text,
+  Button,
+  Image,
+  Box,
+  useSafeArea,
+  Pressable,
+} from "native-base";
+import MasonryList from "@react-native-seoul/masonry-list";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllPosts, selectAllPost } from "../Redux/Slices/postSlice";
+
+const MyFeedScreen = ({ navigation, route }) => {
+  const dispatch = useDispatch();
+
+  const allPost = useSelector(selectAllPost);
+
+  useEffect(() => {
+    dispatch(getAllPosts());
+  }, [dispatch, allPost]);
+
+  const safeAreaProps = useSafeArea({
+    safeAreaTop: true,
+    pt: 2,
+  });
+
+  return allPost ? (
+    <ScrollView {...safeAreaProps}></ScrollView>
+  ) : (
+    <Text>Loading...</Text>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default MyFeedScreen;
