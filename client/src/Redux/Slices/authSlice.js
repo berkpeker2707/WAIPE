bb2b1c2e7d8f826d2a729f3e782f06cf43c319da
Berk <berkpeker94@gmail.com>
2022-11-28 15:15:41 +0300

    removed source icons from project

--- client/src/Redux/Slices/authSlice.js
+++ client/src/Redux/Slices/authSlice.js
@@ -2,7 +2,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import AsyncStorege from "@react-native-async-storage/async-storage";
 import axios from "axios";
 
-const SERVER_URL = "http://192.168.1.38:1000/api";
+const SERVER_URL = "http://192.168.100.21:1000/api";
 
 export const signup = createAsyncThunk(
   "auth/signup",
