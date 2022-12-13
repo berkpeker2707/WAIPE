862c6eeb91f9efb75524da1fa675fe8487e18951
Berk <berkpeker94@gmail.com>
2022-12-13 16:53:24 +0300

    added formData lib to server

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const passport = require("passport");
 const session = require("express-session");
 const dbConnect = require("./config/db/dbConnect");
+const formData = require("express-form-data");
 const cors = require("cors");
 require("dotenv").config();
 require("./config/passport")(passport);
@@ -19,6 +20,7 @@ const app = express();
 
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json());
+app.use(formData.parse());
 
 //database connection
 dbConnect();
