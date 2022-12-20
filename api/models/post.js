c0f8c466a6528b25f3e04ca872394848a9f9c198
Berk <berkpeker94@gmail.com>
2022-12-20 10:28:40 +0300

    changed post model for picture

--- api/models/post.js
+++ api/models/post.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const PostSchema = mongoose.Schema({
   petID: { type: mongoose.Schema.Types.ObjectId, ref: "pet", required: true },
-  postImage: { type: String, required: true },
+  picture: { type: String, required: true },
   postDescription: { type: String },
   like: { type: mongoose.Schema.Types.ObjectId, ref: "like" },
   comment: { type: mongoose.Schema.Types.ObjectId, ref: "comment" },
