aa572042b785893c822b850c64d7b9f100612608
Berk <berkpeker94@gmail.com>
2022-12-29 16:50:36 +0300

    fixed icons

--- client/src/Components/Icons/AddCommentIcon.js
+++ client/src/Components/Icons/AddCommentIcon.js
@@ -2,14 +2,14 @@ import { View } from "native-base";
 import React from "react";
 import Svg, { G, Path } from "react-native-svg";
 
-const AddCommentIcon = () => {
+const AddCommentIcon = (color) => {
   return (
     <View>
       <Svg width={20} height={20} viewBox="0 0 512 512">
         <G>
           <Path
             d="M490.667,234.667H277.333V21.333C277.333,9.551,267.782,0,256,0c-11.782,0-21.333,9.551-21.333,21.333v213.333H21.333   C9.551,234.667,0,244.218,0,256c0,11.782,9.551,21.333,21.333,21.333h213.333v213.333c0,11.782,9.551,21.333,21.333,21.333   c11.782,0,21.333-9.551,21.333-21.333V277.333h213.333c11.782,0,21.333-9.551,21.333-21.333   C512,244.218,502.449,234.667,490.667,234.667z"
-            fill="#C6C6C8"
+            fill={color.color}
           />
         </G>
       </Svg>
