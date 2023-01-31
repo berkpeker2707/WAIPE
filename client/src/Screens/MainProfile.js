0bf5c5308cf6c336be3a01e282e0cbd67e6f6ff4
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-01-31 17:26:49 +0300

    Chnage pet card and pets

--- client/src/Screens/MainProfile.js
+++ client/src/Screens/MainProfile.js
@@ -30,6 +30,14 @@ const MainProfileScreen = ({ navigation }) => {
   const userLoading = useSelector(selectUserLoading);
   const isUpdate = useSelector(selectUserUpdated);
 
+  const infoText = {
+    country: currentUser?.locations?.country
+      ? currentUser?.locations?.country
+      : "",
+    city: currentUser?.locations?.city ? currentUser?.locations?.city : "",
+    biography: currentUser?.biography ? currentUser?.biography : "",
+  };
+
   useEffect(() => {
     dispatch(getCurrentUserAction());
 
@@ -44,12 +52,14 @@ const MainProfileScreen = ({ navigation }) => {
       loading={userLoading}
       name={`${currentUser?.firstname} ${currentUser?.lastname}`}
       pictureUrl={currentUser?.picture}
-      infoText={`${currentUser?.locations?.country}, ${
-        currentUser?.locations?.city
-      }${"\n"}${currentUser?.biography}`}
+      infoText={
+        (infoText && infoText.country) || infoText.city || infoText.biography
+          ? `${infoText.country}, ${infoText.city}${"\n"}${infoText.biography}`
+          : "Empty like my heart..."
+      }
       editPage={"EditMainProfile"}
     >
-      <HStack pl="1" flex="1" flexWrap="wrap" justifyContent="space-between">
+      <HStack pl="1" flex="1" flexWrap="wrap">
         {currentUser?.pets?.map((pet, index) => {
           return (
             <PetCard
@@ -67,6 +77,7 @@ const MainProfileScreen = ({ navigation }) => {
           colorScheme="warning"
           width={100}
           height={100}
+          m="4px"
           icon={
             <Icon
               as={SimpleLineIcons}
