831669fb52329fed59ca119af7b5a62beaa66c7c
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-02-02 17:16:18 +0300

    Add user page

--- client/src/Screens/UserProfile.js
+++ client/src/Screens/UserProfile.js
@@ -1,57 +1,54 @@
 import React, { useEffect } from "react";
-import { SimpleLineIcons } from "@expo/vector-icons";
 import PetCard from "../Components/PetCard";
-import { Icon, HStack, IconButton, useTheme } from "native-base";
+import { HStack } from "native-base";
 import {
-  selectCurrentUser,
   selectUserLoading,
   selectUserUpdated,
-  getCurrentUserAction,
+  selectUser,
+  getUserAction,
 } from "../Redux/Slices/userSlice";
 import { useSelector, useDispatch } from "react-redux";
 import ProfilePage from "../Components/ProfilePage";
 
 const UserProfileScreen = ({ navigation, route }) => {
-  const theme = useTheme();
   const dispatch = useDispatch();
-  const { userID } = route.params;
-  console.log(userID);
+  const userID = route.params.userID;
 
-  const currentUser = useSelector(selectCurrentUser);
+  const user = useSelector(selectUser);
+
   const userLoading = useSelector(selectUserLoading);
   const isUpdate = useSelector(selectUserUpdated);
 
   const infoText = {
-    country: currentUser?.locations?.country
-      ? currentUser?.locations?.country
-      : "",
-    city: currentUser?.locations?.city ? currentUser?.locations?.city : "",
-    biography: currentUser?.biography ? currentUser?.biography : "",
+    country: user?.locations?.country ? user?.locations?.country : "",
+    city: user?.locations?.city ? user?.locations?.city : "",
+    biography: user?.biography ? user?.biography : "",
   };
 
   useEffect(() => {
-    dispatch(getCurrentUserAction());
+    dispatch(getUserAction(userID));
 
     return () => {
       //clean up function
     };
-  }, [dispatch, currentUser?._id, isUpdate]);
+  }, [dispatch, user?._id, isUpdate]);
 
   return (
     <ProfilePage
       navigation={navigation}
       loading={userLoading}
-      name={`${currentUser?.firstname} ${currentUser?.lastname}`}
-      pictureUrl={currentUser?.picture}
+      name={`${user?.firstname} ${user?.lastname}`}
+      pictureUrl={user?.picture}
       infoText={
         (infoText && infoText.country) || infoText.city || infoText.biography
           ? `${infoText.country}, ${infoText.city}${"\n"}${infoText.biography}`
           : "Empty like my heart..."
       }
       editPage={"EditMainProfile"}
+      isCurrentUser={false}
     >
       <HStack pl="1" flex="1" flexWrap="wrap">
-        {currentUser?.pets?.map((pet, index) => {
+        {user?.pets?.map((pet, index) => {
           return (
             <PetCard
               key={index}
@@ -62,22 +59,6 @@ const UserProfileScreen = ({ navigation, route }) => {
             />
           );
         })}
-        <IconButton
-          borderRadius="70"
-          variant="ghost"
-          colorScheme="warning"
-          width={100}
-          height={100}
-          m="4px"
-          icon={
-            <Icon
-              as={SimpleLineIcons}
-              name="plus"
-              size="5xl"
-              color={theme.colors.trueGray[50]}
-            />
-          }
-        />
       </HStack>
     </ProfilePage>
   );
