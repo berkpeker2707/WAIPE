dd342be761a34b1e426ddce9eb130234ee723329
Berk <berkpeker94@gmail.com>
2023-02-27 17:15:21 +0300

    added loading and uuid to myfeed

--- client/src/Screens/MyFeed.js
+++ client/src/Screens/MyFeed.js
@@ -13,12 +13,16 @@ import {
   HStack,
   Circle,
   Divider,
+  Spinner,
+  useSafeArea,
 } from "native-base";
 import { useDispatch, useSelector } from "react-redux";
 import {
   selectGetFollowedPosts,
   getFollowedPostsAction,
+  selectPostLoading,
 } from "../Redux/Slices/postSlice";
+import uuid from "react-native-uuid";
 
 import ReportIcon from "../Components/Icons/ReportIcon";
 import BookmarkIcon from "../Components/Icons/BookmarkIcon";
@@ -28,12 +32,15 @@ const MyFeedScreen = ({ navigation, route }) => {
   const dispatch = useDispatch();
 
   const followedPosts = useSelector(selectGetFollowedPosts);
+  const postLoading = useSelector(selectPostLoading);
 
   useEffect(() => {
+    const controller = new AbortController();
+
     dispatch(getFollowedPostsAction());
 
     return () => {
-      //clean up function
+      controller.abort();
     };
   }, [dispatch]);
 
@@ -53,125 +60,132 @@ const MyFeedScreen = ({ navigation, route }) => {
     };
   }, []);
 
-  return followedPosts ? (
-    <ScrollView bg={theme.colors.sage[400]}>
-      {followedPosts.map((fP, followedPostsIndex) => {
-        return (
-          <Box
-            key={followedPostsIndex}
-            safeAreaTop
-            ml={7}
-            mr={7}
-            style={theme.postShadow}
-          >
-            <Pressable
-              onPress={() => {
-                navigation.navigate("Post", {
-                  post: fP,
-                });
-              }}
-              onLongPress={() => {
-                onLongPressState
-                  ? (setOnLongPressState(() => false),
-                    setOnLongPressItemState(() => null))
-                  : (setOnLongPressState(() => true),
-                    setOnLongPressItemState(() => fP._id));
-              }}
-            >
-              <Box
-                key={followedPostsIndex}
-                maxW="100%"
-                rounded="3xl"
-                overflow="hidden"
-                borderColor={
-                  onLongPressItemState === fP._id && onLongPressState
-                    ? "#E38E48"
-                    : theme.colors.forestGreen[400]
-                }
-                borderWidth="3.5"
-              >
-                <AspectRatio w="100%" ratio={1 / 1}>
-                  <Image
-                    source={{
-                      uri: fP.picture,
-                    }}
-                    alt="image"
-                    blurRadius={
-                      onLongPressItemState === fP._id && onLongPressState
-                        ? 50
-                        : 0
-                    }
-                  />
-                </AspectRatio>
+  const safeAreaProps = useSafeArea({
+    safeArea: true,
+    pt: 2,
+  });
 
-                {onLongPressItemState === fP._id && onLongPressState ? (
-                  <HStack
-                    alignItems="center"
-                    textAlign="center"
-                    justifyContent="center"
-                    style={{
-                      margin: "auto",
-                      position: "absolute",
-                      top: 0,
-                      bottom: 0,
-                      left: 0,
-                      right: 0,
-                    }}
-                  >
+  return !postLoading ? (
+    followedPosts ? (
+      <ScrollView bg={theme.colors.sage[400]}>
+        {followedPosts.map((fP, followedPostsIndex) => {
+          return (
+            <Box
+              key={uuid.v4()}
+              safeAreaTop
+              ml={7}
+              mr={7}
+              style={theme.postShadow}
+            >
+              <Pressable
+                onPress={() => {
+                  navigation.navigate("Post", {
+                    post: fP,
+                  });
+                }}
+                onLongPress={() => {
+                  onLongPressState
+                    ? (setOnLongPressState(() => false),
+                      setOnLongPressItemState(() => null))
+                    : (setOnLongPressState(() => true),
+                      setOnLongPressItemState(() => fP._id));
+                }}
+              >
+                <Box
+                  maxW="100%"
+                  rounded="3xl"
+                  overflow="hidden"
+                  borderColor={
+                    onLongPressItemState === fP._id && onLongPressState
+                      ? "#E38E48"
+                      : theme.colors.forestGreen[400]
+                  }
+                  borderWidth="3.5"
+                >
+                  <AspectRatio w="100%" ratio={1 / 1}>
+                    <Image
+                      source={{
+                        uri: fP.picture,
+                      }}
+                      alt="image"
+                      blurRadius={
+                        onLongPressItemState === fP._id && onLongPressState
+                          ? 50
+                          : 0
+                      }
+                    />
+                  </AspectRatio>
+
+                  {onLongPressItemState === fP._id && onLongPressState ? (
                     <HStack
-                      borderWidth="1"
-                      borderRadius="lg"
-                      borderColor={theme.colors.sage[300]}
-                      p="2"
-                      bg={theme.colors.sage[300]}
+                      alignItems="center"
+                      textAlign="center"
+                      justifyContent="center"
+                      style={{
+                        margin: "auto",
+                        position: "absolute",
+                        top: 0,
+                        bottom: 0,
+                        left: 0,
+                        right: 0,
+                      }}
                     >
-                      <Pressable
-                        mr={1}
-                        alignItems="center"
-                        textAlign="center"
-                        justifyContent="center"
-                        onPress={() =>
-                          navigation.navigate("Post", {
-                            post: fP,
-                          })
-                        }
+                      <HStack
+                        borderWidth="1"
+                        borderRadius="lg"
+                        borderColor={theme.colors.sage[300]}
+                        p="2"
+                        bg={theme.colors.sage[300]}
                       >
-                        {({ isHovered, isFocused, isPressed }) => {
-                          return (
-                            <Center
-                              _text={{
-                                color: "black",
-                                fontWeight: "normal",
-                              }}
-                              style={{
-                                transform: [{ scale: isPressed ? 0.96 : 1 }],
-                              }}
-                            >
-                              {fP.petID.name ?? ""}
-                            </Center>
-                          );
-                        }}
-                      </Pressable>
-                      <Pressable mr={1}>
-                        {({ isHovered, isFocused, isPressed }) => {
-                          return (
-                            <Circle
-                              size="30px"
-                              bg={theme.colors.forestGreen[400]}
-                              style={{
-                                transform: [{ scale: isPressed ? 0.96 : 1 }],
-                              }}
-                            >
-                              <Icon
-                                as={
-                                  <ReportIcon color={theme.colors.sage[300]} />
-                                }
-                              />
-                            </Circle>
-                          );
-                        }}
-                      </Pressable>
-                      {/* <Pressable mr={1}>
+                        <Pressable
+                          mr={1}
+                          alignItems="center"
+                          textAlign="center"
+                          justifyContent="center"
+                          onPress={() =>
+                            navigation.navigate("Post", {
+                              post: fP,
+                            })
+                          }
+                        >
+                          {({ isHovered, isFocused, isPressed }) => {
+                            return (
+                              <Center
+                                _text={{
+                                  color: "black",
+                                  fontWeight: "normal",
+                                }}
+                                style={{
+                                  transform: [{ scale: isPressed ? 0.96 : 1 }],
+                                }}
+                              >
+                                {fP.petID.name ?? ""}
+                              </Center>
+                            );
+                          }}
+                        </Pressable>
+                        <Pressable mr={1}>
+                          {({ isHovered, isFocused, isPressed }) => {
+                            return (
+                              <Circle
+                                size="30px"
+                                bg={theme.colors.forestGreen[400]}
+                                style={{
+                                  transform: [{ scale: isPressed ? 0.96 : 1 }],
+                                }}
+                              >
+                                <Icon
+                                  as={
+                                    <ReportIcon
+                                      color={theme.colors.sage[300]}
+                                    />
+                                  }
+                                />
+                              </Circle>
+                            );
+                          }}
+                        </Pressable>
+                        {/* <Pressable mr={1}>
                         {({ isHovered, isFocused, isPressed }) => {
                           return (
                             <Circle
@@ -192,41 +206,50 @@ const MyFeedScreen = ({ navigation, route }) => {
                           );
                         }}
                       </Pressable> */}
-                      <Pressable>
-                        {({ isHovered, isFocused, isPressed }) => {
-                          return (
-                            <Circle
-                              size="30px"
-                              bg={theme.colors.forestGreen[400]}
-                              style={{
-                                transform: [{ scale: isPressed ? 0.96 : 1 }],
-                              }}
-                            >
-                              <Icon
-                                as={
-                                  <BookmarkIcon
-                                    color={theme.colors.sage[300]}
-                                  />
-                                }
-                              />
-                            </Circle>
-                          );
-                        }}
-                      </Pressable>
+                        <Pressable>
+                          {({ isHovered, isFocused, isPressed }) => {
+                            return (
+                              <Circle
+                                size="30px"
+                                bg={theme.colors.forestGreen[400]}
+                                style={{
+                                  transform: [{ scale: isPressed ? 0.96 : 1 }],
+                                }}
+                              >
+                                <Icon
+                                  as={
+                                    <BookmarkIcon
+                                      color={theme.colors.sage[300]}
+                                    />
+                                  }
+                                />
+                              </Circle>
+                            );
+                          }}
+                        </Pressable>
+                      </HStack>
                     </HStack>
-                  </HStack>
-                ) : (
-                  <></>
-                )}
-              </Box>
-            </Pressable>
-          </Box>
-        );
-      })}
-      <Box alignItems="center">
-        <Divider bg={theme.colors.sage[300]} mt="5" mb="5" w="60%" />
-      </Box>
-    </ScrollView>
+                  ) : (
+                    <></>
+                  )}
+                </Box>
+              </Pressable>
+            </Box>
+          );
+        })}
+        <Box alignItems="center">
+          <Divider bg={theme.colors.sage[300]} mt="5" mb="5" w="60%" />
+        </Box>
+      </ScrollView>
+    ) : (
+      <ScrollView
+        bg={theme.colors.sage[400]}
+        {...safeAreaProps}
+        contentContainerStyle={{ flexGrow: 1, justifyContent: "center" }}
+      >
+        <Spinner color={"mustard.400"} size="lg" />
+      </ScrollView>
+    )
   ) : (
     <ScrollView bg={theme.colors.sage[400]}>
       <Stack safeArea>
