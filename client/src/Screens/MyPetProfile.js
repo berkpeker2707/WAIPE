83717412e93f76aca75d6e03156dc8bceb809123
Doğa Özsöyler <doga.ozsoyler@gmail.com>
2023-03-07 13:25:43 +0300

    Add scrollView for background

--- client/src/Screens/MyPetProfile.js
+++ client/src/Screens/MyPetProfile.js
@@ -1,5 +1,5 @@
 import React, { useEffect, memo } from "react";
-import { HStack, Image, ScrollView, useSafeArea } from "native-base";
+import { HStack, Image, ScrollView, useTheme } from "native-base";
 import {
   selectGetPet,
   selectPetLoading,
@@ -9,17 +9,17 @@ import {
 import { useSelector, useDispatch } from "react-redux";
 import PressableButton from "../Components/PressableButton";
 import ProfilePage from "../Components/ProfilePage";
+import { selectCurrentUser } from "../Redux/Slices/userSlice";
 
 const MyPetProfile = memo(({ navigation, route }) => {
   const { petId } = route.params;
   const dispatch = useDispatch();
+  const theme = useTheme();
 
   const pet = useSelector(selectGetPet);
   const petLoading = useSelector(selectPetLoading);
   const isUpdate = useSelector(selectPetUpdated);
-  console.log("pet");
-  console.log(pet?.ownerID.firstname);
-  console.log("pet");
+  const currentUser = useSelector(selectCurrentUser);
 
   useEffect(() => {
     dispatch(getPetAction(petId));
@@ -30,43 +30,50 @@ const MyPetProfile = memo(({ navigation, route }) => {
   }, [dispatch, petId, isUpdate]);
 
   return (
-    <ProfilePage
-      navigation={navigation}
-      loading={petLoading}
-      name={`${pet?.name}`}
-      pictureUrl={pet?.picture}
-      infoText={`${pet?.species}, ${pet?.age}${"\n"}${
-        pet?.interestedIn
-      }${"\n"}${pet?.biography}`}
-      editPage={"EditPetProfile"}
-      isCurrentUser={true}
-      user={pet?.ownerID}
+    <ScrollView
+      bg={theme.colors.sage[400]}
+      contentContainerStyle={{
+        flexGrow: 1,
+      }}
     >
-      <HStack flex="1" flexWrap="wrap" justifyContent="space-between">
-        {pet?.petPost?.map((post, index) => {
-          return (
-            <PressableButton
-              key={index}
-              onPress={() => navigation.navigate("Post", { post: post })}
-            >
-              <Image
-                source={{
-                  uri: `${post.picture}`,
-                }}
-                alt="Alternate Text"
-                size="xl"
-                w={160}
-                mr={(index + 1) % 2 === 0 ? "0" : "1"}
-                ml={(index + 1) % 2 !== 0 ? "0" : "1"}
-                mt="1"
-                mb="1"
-                borderRadius="xl"
-              />
-            </PressableButton>
-          );
-        })}
-      </HStack>
-    </ProfilePage>
+      <ProfilePage
+        navigation={navigation}
+        loading={petLoading}
+        name={`${pet?.name}`}
+        pictureUrl={pet?.picture}
+        infoText={`${pet?.species}, ${pet?.age}${"\n"}${
+          pet?.interestedIn
+        }${"\n"}${pet?.biography}`}
+        editPage={"EditPetProfile"}
+        isCurrentUser={pet?.ownerID?._id === currentUser._id}
+        user={pet?.ownerID}
+      >
+        <HStack flex="1" flexWrap="wrap" justifyContent="space-between">
+          {pet?.petPost?.map((post, index) => {
+            return (
+              <PressableButton
+                key={index}
+                onPress={() => navigation.navigate("Post", { post: post })}
+              >
+                <Image
+                  source={{
+                    uri: `${post.picture}`,
+                  }}
+                  alt="Alternate Text"
+                  size="xl"
+                  w={160}
+                  mr={(index + 1) % 2 === 0 ? "0" : "1"}
+                  ml={(index + 1) % 2 !== 0 ? "0" : "1"}
+                  mt="1"
+                  mb="1"
+                  borderRadius="xl"
+                />
+              </PressableButton>
+            );
+          })}
+        </HStack>
+      </ProfilePage>
+    </ScrollView>
   );
 });
 
