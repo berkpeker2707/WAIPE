f9dda79c0ea20b0c7f3069b3b30902c621b8fd62
Berk <berkpeker94@gmail.com>
2023-03-14 13:20:51 +0300

    removed unused imports

--- client/src/Screens/NewPost.js
+++ client/src/Screens/NewPost.js
@@ -1,81 +1,17 @@
 import React, { useEffect, useMemo, useState } from "react";
-import { View, StyleSheet } from "react-native";
-import {
-  ScrollView,
-  Box,
-  AspectRatio,
-  Image,
-  Text,
-  VStack,
-  HStack,
-  Stack,
-  Divider,
-  Circle,
-  Icon,
-  Center,
-  Pressable,
-  TextArea,
-  Avatar,
-  useTheme,
-  Button,
-} from "native-base";
-
-import uuid from "react-native-uuid";
-import LikeHeartIcon from "../Components/Icons/LikeHeartIcon";
-import AddCommentIcon from "../Components/Icons/AddCommentIcon";
-import SendMessageIcon from "../Components/Icons/SendMessageIcon";
-import CuteCatFeverCoffeeIcon from "../Components/Icons/CuteCatFeverCoffeeIcon";
-import CuteCowSurprisedIcon from "../Components/Icons/CuteCowSurprisedIcon";
-import CuteRabbitHoldingCarrotIcon from "../Components/Icons/CuteRabbitHoldingCarrotIcon";
-import CuteSadCatSittingIcon from "../Components/Icons/CuteSadCatSittingIcon";
-
-import ReportIcon from "../Components/Icons/ReportIcon";
-import BookmarkIcon from "../Components/Icons/BookmarkIcon";
+import { ScrollView, Text, Stack, useTheme } from "native-base";
 import { useDispatch, useSelector } from "react-redux";
-
-import {
-  archivePostAction,
-  getPostAction,
-  selectGetPost,
-  selectPostUpdated,
-} from "../Redux/Slices/postSlice";
-import {
-  getCommentAction,
-  selectCommentUpdated,
-  selectGetComment,
-  updateCommentAction,
-} from "../Redux/Slices/commentSlice";
-import { selectLikeUpdated } from "../Redux/Slices/likeSlice";
-
-import PostImageSection from "../Components/PostComponents/PostImageSection";
-import PostImageLikeSection from "../Components/PostComponents/PostImageLikeSection";
-import PostViewCommentSection from "../Components/PostComponents/PostViewCommentSection";
 import {
   getCurrentUserAction,
   selectCurrentUser,
 } from "../Redux/Slices/userSlice";
-
 import NewPostUnifiedSection from "../Components/NewPostComponents/NewPostUnifiedSection";
 
 const NewPostScreen = ({ navigation, route }) => {
   const theme = useTheme();
   const dispatch = useDispatch();
 
-  // const getPostState = useSelector(selectGetPost);
-  // const getCommentState = useSelector(selectGetComment);
-
-  // const isLikeUpdated = useSelector(selectLikeUpdated);
-  // const isCommentUpdated = useSelector(selectCommentUpdated);
-
   const currentUser = useSelector(selectCurrentUser);
-
-  // useEffect(() => {
-  //   dispatch(getPostAction(route.params.post._id));
-
-  //   return () => {
-  //     //clean up function
-  //   };
-  // }, [dispatch, route.params.post._id, isLikeUpdated]);
 
   useEffect(() => {
     dispatch(getCurrentUserAction());
@@ -84,14 +20,6 @@ const NewPostScreen = ({ navigation, route }) => {
       //clean up function
     };
   }, [dispatch]);
-
-  // useEffect(() => {
-  //   dispatch(getCommentAction(route.params.post.comment._id));
-
-  //   return () => {
-  //     //clean up function
-  //   };
-  // }, [dispatch, route.params.post.comment._id, isCommentUpdated]);
 
   return currentUser && currentUser._id ? (
     <ScrollView bg={theme.colors.sage[400]}>
