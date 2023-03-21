bc6c4af0252abe0aa1986847103187092ee32204
Berk <berkpeker94@gmail.com>
2023-03-21 13:16:40 +0300

    fixed Discover video displaying with tumbnail

--- client/src/Components/DiscoverMasonryListRenderedComponent.js
+++ client/src/Components/DiscoverMasonryListRenderedComponent.js
@@ -1,5 +1,4 @@
 import React, { useMemo } from "react";
-import MasonryList from "@react-native-seoul/masonry-list";
 import { Image, Pressable, View } from "native-base";
 
 const DiscoverMasonryListRenderedComponent = (props) => {
@@ -19,20 +18,29 @@ const DiscoverMasonryListRenderedComponent = (props) => {
       }}
     >
       <View key={item._id} style={[{ marginTop: 12, flex: 1 }]}>
-        {item && item.picture ? (
+        {(item && item.picture.includes(".mp4")) ||
+        item.picture.includes(".avi") ||
+        item.picture.includes(".mov") ? (
           <Image
-            source={{ uri: item.picture ? item.picture : null }}
+            source={{
+              uri: item.picture
+                ? item.picture
+                    .replace(/.mp4/g, ".jpg")
+                    .replace(/.avi/g, ".jpg")
+                    .replace(/.mov/g, ".jpg")
+                : null,
+            }}
             style={{
               height: randomBool ? 150 : 280,
               alignSelf: "stretch",
               marginLeft: i % 2 === 0 ? 0 : 12,
             }}
             resizeMode="cover"
-            alt="alt"
+            alt="image"
           />
         ) : (
           <Image
-            source={require("../../assets/icon.png")}
+            source={{ uri: item.picture ? item.picture : null }}
             style={{
               height: randomBool ? 150 : 280,
               alignSelf: "stretch",
